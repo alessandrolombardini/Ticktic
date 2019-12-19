620e610ae47de029d2945b2e55e6710db7c0d933
Alessandro Lombardini - alessandr.lombardin3@studio.unibo.it <alessandr.lombardin3@studio.unibo.it>
2019-12-19 18:59:49 +0100

    Add

---
+++ Sito/gestisci_articoli.php
@@ -0,0 +1,5 @@
+<?php
+    $templateParams["page_content"] = "./template/gestisci_articoli_content.php";
+
+    require_once("./template/base.php");
+?>
---
+++ Sito/template/biglietto_content.php
@@ -0,0 +1,34 @@
+<div class=" m-0 mt-3 ">
+    <div class="row">
+        <div class="container-fluid col-md-5">
+            <img class="img-fluid" src="./images/artisti/foo_fighters.jpg" alt="foo fighters"/>
+        </div>
+        <div class="col-md-7 text-center">
+            <h3>IDAYS - Foo Fighters</h3>
+            <p>I Foo Figers sono un gruppo musicale lternative-rock statunitense fondato a Seattle nel 1994.</p>
+            <h4>20/05/19 ore 20:00 - Milano</h4>
+            <h5>Milano Innovation district(EXPO)</h5> 
+            <hr class="mb-3"></hr>
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="chart-container col-10 bg-white border mt-2 mb-4 px-4 py-3 shadow-sm">
+                    <div class="row mt-3">
+                        <p class="col-3 mb-0 mt-2 text-center"> Parterre </p>
+                        <p class="col-3 mb-0 mt-2 col-md-4 text-right pr-1"> cad.</p>
+                        <p class="col-3 mb-0 mt-2 col-md-2 text-left p-0"> € 19,50</p>
+                        <i class="col-1 fas fa-plus-circle fa-2x text-right mb-3 px-0 pb-0 pt-1"></i>
+                        <p class="col-1 text-center p-0 m-0 tickets-number">1</p>
+                        <i class="col-1 fas fa-minus-circle fa-2x text-left mb-3 px-0 pb-0 pt-1"></i>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-8"></div>
+                        <a id="testo-responsive" href="./artista.php?id=1" class="btn col-4 mr-0 text-center rounded-pill">Visualizza</a>
+
+                    </div>
+                    <hr />
+                </div>
+            <div class="col-1"></div>
+            </div>
+        </div>
+    </div>
+</div>
