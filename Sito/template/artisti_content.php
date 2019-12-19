591a6d8ea8939da4e019fd8bf3393cd896ec9fd5
Martina <martina.baiardi4@studio.unibo.it>
2019-12-19 16:47:50 +0100

    Aggiunta e linkata la pagina per visualizzare tutti gli artisti

--- Sito/template/artisti_content.php
+++ Sito/template/artisti_content.php
@@ -1,31 +1,36 @@
-<div class="container ">
-    <br />
-    <h3 class ="d-inline">Artisti</h3> <p class="text-black-50 d-inline">(2 Risultati)</p>
-    <div class="dropdown-divider"></div>
-    <br />
-    <table class="table table-hover">
+<div class="container mt-3">
+    <h3 class ="d-inline">Artisti</h3> 
+    <p class="text-black-50 d-inline">(2 Risultati)</p>
+    <div class="mb-3 dropdown-divider"></div>
+
+    <table class="table table-hover mt-2 mb-2" >
         <tbody>
             <tr>
-                <td class="col-5 col-md-2 align-middle">
+                <td class="align-middle">
                     <img src="./images/artisti/foo_fighters.jpg" class="mx-2 img-fluid" alt="foo fighters" />
                 </td>
-                <td class="col-6 col-md-8 align-middle">
-                    <p>Foo Fighters</p>
-                    <p class="text-black-50 description">I Foo Fighters sono un gruppo musicale alternative rock statunitense fondato a Seattle nel 1994 dal musicista Dave Grohl, ex batterista dei Nirvana.</p>
+                <td class="w-75 align-middle">
+                    <p id="testo-responsive"> Foo Fighters</p>
+                    <p id="testo-responsive" class="text-black-50 description">I Foo Fighters sono un gruppo musicale alternative rock statunitense fondato a Seattle nel 1994 dal musicista Dave Grohl, ex batterista dei Nirvana.</p>
+                    <div class=" text-right">
+                        <a id="testo-responsive" href="./artista.php?id=1" class="btn text-light shadow-sm p-1 mt-1 pr-3 pl-3 rounded-pill description">Visualizza</a>
+                    </div>
                 </td>
             </tr>
-            <tr class="">
-                <td class="col-5 col-md-2 align-middle">
+            <tr>
+                <td class="align-middle">
                     <img src="./images/artisti/salmo.jpg" class="mx-2 img-fluid" alt="foo fighters" />
                 </td>
-                <td class="col-6 col-md-8 align-middle">
-                    <p>Salmo</p>
-                    <p class="text-black-50 description">Salmo, pseudonimo di Maurizio Pisciottu (Olbia, 29 giugno 1984), è un rapper e produttore discografico italiano. Salmo ha iniziato la sua carriera musicale all'età di 13 anni, incidendo le sue prime rime tra il 1997 e il 1998.</p>
+                <td class="w-75 align-middle">
+                    <p id="testo-responsive">Salmo</p>
+                    <p id="testo-responsive" class="text-black-50 description">Salmo, pseudonimo di Maurizio Pisciottu (Olbia, 29 giugno 1984), è un rapper e produttore discografico italiano. Salmo ha iniziato la sua carriera musicale all'età di 13 anni, incidendo le sue prime rime tra il 1997 e il 1998.</p>
+                    <div class=" text-right">
+                        <a id="testo-responsive" href="./artista.php?id=2" class="btn text-light shadow-sm p-1 mt-1 pr-3 pl-3 rounded-pill description"> Visualizza</a>
+                    </div>
                 </td>
             </tr>
         </tbody>
-</table>
-
-    <br />
+    </table>
+    <div class="dropdown-divider mb-3"></div>
     
 </div>
