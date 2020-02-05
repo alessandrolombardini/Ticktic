50ed9315ab0a7853c84e87b7f7fcd9d080024b37
Alessandro Lombardini - alessandr.lombardin3@studio.unibo.it <alessandr.lombardin3@studio.unibo.it>
2020-02-05 09:55:21 +0100

    Fix

--- Sito/db/db.php
+++ Sito/db/db.php
@@ -187,7 +187,7 @@
         $stmt->execute();
         return $stmt->get_result();
     }
-
+    
     public function aggiungiBigliettiAdEventoInCarrello($IDEvento, $IDUtente, $numeroPrecedenteDiBiglietti, $bigliettiDaAggiungere){
         $numeroBigliettiNuovo = $numeroPrecedenteDiBiglietti + $bigliettiDaAggiungere;
         $query = "UPDATE DESIDERA_ACQUISTARE
@@ -496,52 +496,6 @@
         } else {
             return false;
         }
-    }
-
-    /********************************************************************************************************************** */
-    /* Acquisto */
-    public function aggiungiACarrelloEvento($IDEvento, $bigliettiDaAggiungere, $IDUtente){
-        $result = $this->verificaPresenzaEventoInCarrelloDiUtente($IDEvento, $IDUtente);
-        if($result->num_rows > 0){
-            $numeroPrecedenteDiBiglietti = $result->fetch_assoc()["NumeroBiglietti"];
-            $this->aggiungiBigliettiAdEventoInCarrello($IDEvento, $IDUtente, $numeroPrecedenteDiBiglietti, $bigliettiDaAggiungere);
-        } else {
-            $query = "INSERT INTO DESIDERA_ACQUISTARE(IDEvento, NumeroBiglietti, IDUtente)
-                      VALUES (?,?,?)";
-            $stmt = $this->db->prepare($query);
-            $stmt->bind_param('sss', $IDEvento, $bigliettiDaAggiungere, $IDUtente);
-            $stmt->execute();
-            return $stmt->insert_id;
-        }
-    }
-
-    public function verificaPresenzaEventoInCarrelloDiUtente($IDEvento, $IDUtente){
-        $query = "SELECT *
-                  FROM DESIDERA_ACQUISTARE
-                  WHERE IDEvento = ? AND IDUtente = ?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ss', $IDEvento, $IDUtente);
-        $stmt->execute();
-        return $stmt->get_result();
-    }
-
-    public function aggiungiBigliettiAdEventoInCarrello($IDEvento, $IDUtente, $numeroPrecedenteDiBiglietti, $bigliettiDaAggiungere){
-        $numeroBigliettiNuovo = $numeroPrecedenteDiBiglietti + $bigliettiDaAggiungere;
-        $query = "UPDATE DESIDERA_ACQUISTARE
-                  SET NumeroBiglietti = ?
-                  WHERE IDEvento = ? && IDUtente = ?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('iii', $numeroBigliettiNuovo, $IDEvento, $IDUtente);
-        $stmt->execute();
-    }
-
-    public function rimuoviEventoDalCarrello($IDEvento, $IDUtente){
-        $query = "DELETE FROM DESIDERA_ACQUISTARE
-                  WHERE IDEvento = ? && IDUtente = ?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ii', $IDEvento, $IDUtente);
-        $stmt->execute();
-        return $stmt->insert_id;
     }
     /********************************************************************************************************************** */
 
