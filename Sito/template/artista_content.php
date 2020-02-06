14422fbb8ade008e3f8625cb97258795a8f39a40
Martina <martina.baiardi4@studio.unibo.it>
2020-02-06 13:39:15 +0100

    Tentativo di migliorare i box evento

--- Sito/template/artista_content.php
+++ Sito/template/artista_content.php
@@ -1,110 +1,112 @@
-<div class="container mt-3">
-    <div class="container-fluid text-center "> <!-- Foto e descrizione del gruppo musicale -->
-        <img class="img-fluid w-50 rounded-circle mb-2" src="./images/artisti/foo_fighters.jpg" alt="foo fighters"/>
-        <h3>Foo Fighters</h3>
-        <p class="text-black-50 font-description  close-lines mb-3 px-5">I Foo Fighters sono un gruppo musicale alternative rock statunitense fondato a Seattle nel 1994 dal musicista Dave Grohl, ex batterista dei Nirvana.</p>
+<div class="row mt-3">
+    <div class="col-1 col-md-2"></div>
+    <div class="col-10 col-md-8">
+        <div class="container-fluid text-center "> <!-- Foto e descrizione del gruppo musicale -->
+            <img class="img-fluid w-50 rounded-circle mb-2" src="./images/artisti/foo_fighters.jpg" alt="foo fighters"/>
+            <h3>Foo Fighters</h3>
+            <p class="text-black-50 font-description  close-lines mb-3 px-5">I Foo Fighters sono un gruppo musicale alternative rock statunitense fondato a Seattle nel 1994 dal musicista Dave Grohl, ex batterista dei Nirvana.</p>
+        </div>
+        <div class="my-3"> <!-- Intestazione eventi dell'artista -->
+            <h3 class ="d-inline">Eventi Artista</h3> 
+            <p class="text-black-50 d-inline">(1 Risultato)</p>
+            <div class="dropdown-divider"></div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-xl-6">
+                <div class="card m-1" >
+                    <div class="card-body">
+                        <div class="row h-100 justify-content-beetween no-gutters">
+                            <div class="col-5 col-sm-7 col-xl-5 mt-xl-2 mt-md-2">
+                                <img class="img-fluid rounded" src="./images/eventi/salmo.jpg" alt="salmo"/>
+                            </div>
+
+                            <div class="col-auto ml-auto mt-sm-3 mt-xl-1 mt-md-5 mr-3 mr-md-3">
+                                <div class="d-inline-block text-center">
+                                    <p class="mb-0 ">IDAYS - Foo Fighters</p>
+                                    <p class="font-description close-lines text-black-50 m-0">Data evento 13/09/19</p>
+                                </div>
+
+                                <div class="text-left mt-1 row ml-3">
+                                    <button class="btn py-1 px-3 shadow-sm purple-btn rounded-pill">Scopri</button>
+                                    <a href="#" class="link text-dark "><span class="mx-3 far fa-heart fa-2x"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+            
+            <div class="col-12 col-xl-6">
+                <div class="card m-1" >
+                    <div class="card-body">
+                        <div class="row h-100 justify-content-beetween no-gutters">
+                            <div class="col-5 col-sm-7 col-xl-5 mt-xl-2 mt-md-2">
+                                <img class="img-fluid rounded" src="./images/eventi/salmo-estere-biglietti.jpg" alt="salmo"/>
+                            </div>
+
+                            <div class="col-auto ml-auto mt-sm-3 mt-xl-1 mt-md-5 mr-3 mr-md-3">
+                                <div class="d-inline-block text-center">
+                                    <p class="mb-0 ">IDAYS - Foo Fighters</p>
+                                    <p class="font-description close-lines text-black-50 m-0">Data evento 13/09/19</p>
+                                </div>
+
+                                <div class="text-left mt-1 row ml-3">
+                                    <button class="btn py-1 px-3 shadow-sm purple-btn rounded-pill">Scopri</button>
+                                    <a href="#" class="link text-dark "><span class="mx-3 far fa-heart fa-2x"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-xl-6">
+                <div class="card m-1" >
+                    <div class="card-body">
+                        <div class="row h-100 justify-content-beetween no-gutters">
+                            <div class="col-5 col-sm-7 col-xl-5 mt-xl-2 mt-md-2">
+                                <img class="img-fluid rounded" src="./images/eventi/salmo.jpg" alt="salmo"/>
+                            </div>
+
+                            <div class="col-auto ml-auto mt-sm-3 mt-xl-1 mt-md-5 mr-3 mr-md-3">
+                                <div class="d-inline-block text-center">
+                                    <p class="mb-0 ">IDAYS - Foo Fighters</p>
+                                    <p class="font-description close-lines text-black-50 m-0">Data evento 13/09/19</p>
+                                </div>
+
+                                <div class="text-left mt-1 row ml-3">
+                                    <button class="btn py-1 px-3 shadow-sm purple-btn rounded-pill">Scopri</button>
+                                    <a href="#" class="link text-dark "><span class="mx-3 far fa-heart fa-2x"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-xl-6">
+                <div class="card m-1" >
+                    <div class="card-body">
+                        <div class="row h-100 justify-content-beetween no-gutters">
+                            <div class="col-5 col-sm-7 col-xl-5 mt-xl-2 mt-md-2">
+                                <img class="img-fluid rounded" src="./images/eventi/salmo.jpg" alt="salmo"/>
+                            </div>
+
+                            <div class="col-auto ml-auto mt-sm-3 mt-xl-1 mt-md-5 mr-3 mr-md-3">
+                                <div class="d-inline-block text-center">
+                                    <p class="mb-0 ">IDAYS - Foo Fighters</p>
+                                    <p class="font-description close-lines text-black-50 m-0">Data evento 13/09/19</p>
+                                </div>
+
+                                <div class="text-left mt-1 row ml-3">
+                                    <button class="btn py-1 px-3 shadow-sm purple-btn rounded-pill">Scopri</button>
+                                    <a href="#" class="link text-dark "><span class="mx-3 far fa-heart fa-2x"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
-    <div class="my-3"> <!-- Intestazione eventi dell'artista -->
-        <h3 class ="d-inline">Eventi Artista</h3> 
-        <p class="text-black-50 d-inline">(1 Risultato)</p>
-        <div class="dropdown-divider"></div>
-    </div>
-    <div class="row">
-        <div class="col-12 col-md-6 col-xl-4">
-            <div class="card m-1" >
-                <div class="card-body">
-                    <div class="row h-100 justify-content-beetween no-gutters">
-                        <div class="col-5 col-xl-4 mt-xl-3 mt-md-2">
-                            <img class="img-fluid rounded" src="./images/eventi/salmo.jpg" alt="salmo"/>
-                        </div>
-
-                        <div class="col-auto ml-auto mr-3 mr-md-0">
-                            <div class="d-inline-block text-center">
-                                <p class="mb-0 ">IDAYS - Foo Fighters</p>
-                                <p class="font-description close-lines text-black-50 m-0">Data evento 13/09/19</p>
-                            </div>
-
-                            <div class="text-left mt-1 row ml-3">
-                                <button class="btn py-1 px-3 shadow-sm purple-btn rounded-pill">Scopri</button>
-                                <a href="#" class="link text-dark "><span class="mx-3 far fa-heart fa-2x"></span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <div class="col-12 col-md-6 col-xl-4">
-            <div class="card m-1" >
-                <div class="card-body">
-                    <div class="row h-100 justify-content-beetween no-gutters">
-                        <div class="col-5 col-xl-4 mt-xl-3 mt-md-2">
-                            <img class="img-fluid rounded" src="./images/eventi/salmo.jpg" alt="salmo"/>
-                        </div>
-
-                        <div class="col-auto ml-auto mr-3 mr-md-0">
-                            <div class="d-inline-block text-center">
-                                <p class="mb-0 ">IDAYS - Foo Fighters</p>
-                                <p class="font-description close-lines text-black-50 m-0">Data evento 13/09/19</p>
-                            </div>
-
-                            <div class="text-left mt-1 row ml-3">
-                                <button class="btn py-1 px-3 shadow-sm purple-btn rounded-pill">Scopri</button>
-                                <a href="#" class="link text-dark "><span class="mx-3 far fa-heart fa-2x"></span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-12 col-md-6 col-xl-4">
-            <div class="card m-1" >
-                <div class="card-body">
-                    <div class="row h-100 justify-content-beetween no-gutters">
-                        <div class="col-5 col-xl-4 mt-xl-3 mt-md-2">
-                            <img class="img-fluid rounded" src="./images/eventi/salmo.jpg" alt="salmo"/>
-                        </div>
-
-                        <div class="col-auto ml-auto mr-3 mr-md-0">
-                            <div class="d-inline-block text-center">
-                                <p class="mb-0 ">IDAYS - Foo Fighters</p>
-                                <p class="font-description close-lines text-black-50 m-0">Data evento 13/09/19</p>
-                            </div>
-
-                            <div class="text-left mt-1 row ml-3">
-                                <button class="btn py-1 px-3 shadow-sm purple-btn rounded-pill">Scopri</button>
-                                <a href="#" class="link text-dark "><span class="mx-3 far fa-heart fa-2x"></span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-12 col-md-6 col-xl-4">
-            <div class="card m-1" >
-                <div class="card-body">
-                    <div class="row h-100 justify-content-beetween no-gutters">
-                        <div class="col-5 col-xl-4 mt-xl-3 mt-md-2">
-                            <img class="img-fluid rounded" src="./images/eventi/salmo.jpg" alt="salmo"/>
-                        </div>
-
-                        <div class="col-auto ml-auto mr-3 mr-md-0">
-                            <div class="d-inline-block text-center">
-                                <p class="mb-0 ">IDAYS - Foo Fighters</p>
-                                <p class="font-description close-lines text-black-50 m-0">Data evento 13/09/19</p>
-                            </div>
-
-                            <div class="text-left mt-1 row ml-3">
-                                <button class="btn py-1 px-3 shadow-sm purple-btn rounded-pill">Scopri</button>
-                                <a href="#" class="link text-dark "><span class="mx-3 far fa-heart fa-2x"></span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
+    <div class="col-1 col-md-2"></div>
 </div>
