7bf88b6c9bd29c77408d46a6c135b009ef46d509
Martina <martina.baiardi4@studio.unibo.it>
2020-02-11 11:36:48 +0100

    Nascosta la searchbar del menu dalla homepage

--- Sito/template/base.php
+++ Sito/template/base.php
@@ -28,13 +28,15 @@
                 </a>
 
                 <!-- Search Bar Desktop -->
-                <ul class=" ml-10 navbar-nav align-items-center d-none d-xl-block mr-auto desktop-search-bar">
-                    <li class="nav-item" >
-                        <form class="form-inline search-box" >
-                            <input class="form-control rounded-pill search-input w-100"  type="search" placeholder="Cerca ..." aria-label="Search" />
-                        </form>
-                    </li>
-                </ul>
+                <?php if (!isset($templateParams["hide_search_bar"]) || $templateParams["hide_search_bar"] != "y"):?>
+                    <ul class=" ml-10 navbar-nav align-items-center d-none d-xl-block mr-auto desktop-search-bar">
+                        <li class="nav-item" >
+                            <form class="form-inline search-box" >
+                                <input class="form-control rounded-pill search-input w-100"  type="search" placeholder="Cerca ..." aria-label="Search" />
+                            </form>
+                        </li>
+                    </ul>
+                <?php endif?>
 
                 <!-- Navbar Right Side-->
                 <ul class="navbar nav align-items-center float-right">
@@ -54,19 +56,21 @@
             </nav>
 
             <!-- Search Bar Mobile -->  
-            <div class="row text-center">  
-                <ul class=" mb-2 mx-auto navbar-nav d-block d-xl-none align-items-center d-none mobile-search-bar">
-                    <li class="nav-item" >
-                        <form class="form-inline search-box" >
-                            <input class="form-control rounded-pill search-input w-100"  type="search" placeholder="Cerca ..." aria-label="Search" />
-                        </form>
-                    </li>
-                </ul>
-            </div>
+            <?php if (!isset($templateParams["hide_search_bar"]) || $templateParams["hide_search_bar"] != "y"):?>
+                <div class="row text-center">  
+                    <ul class=" mb-2 mx-auto navbar-nav d-block d-xl-none align-items-center d-none mobile-search-bar">
+                        <li class="nav-item" >
+                            <form class="form-inline search-box" >
+                                <input class="form-control rounded-pill search-input w-100"  type="search" placeholder="Cerca ..." aria-label="Search" />
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            <?php endif ?>
 
             <!-- Nav overlay -->
             <div class="overlay" id="nav"> <!-- Contenuto del menu -->
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#" class="closebtn" onclick="closeNav()">&times;</a>
                 <div class="overlay-content">
                     <a class="font-weight-bold" href="./account.php">Account</a>   
                     <?php if (isset($_SESSION["id"]) && $_SESSION["autorizzazione"] == "ORGANIZZATORE"):?>
