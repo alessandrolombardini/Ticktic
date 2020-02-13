df734bd0fdf38e592d3f371cfe146e9decfad7e6
Martina <martina.baiardi4@studio.unibo.it>
2020-02-13 18:28:35 +0100

    standardizzati tutti i box-evento

--- Sito/template/utente/lista_desideri_content.php
+++ Sito/template/utente/lista_desideri_content.php
@@ -19,18 +19,20 @@
     <?php endif ?>
     <div class="row">
         <?php foreach ($templateParams["eventiListaDesideri"] as $evento) :?>
-            <div class="col-12 col-md-6 col-lg-3 col-xl-3 shadow-sm pl-1 pr-1 ml-0 mr-0 pt-2 pb-2 mt-3 mb-4 bg-white rounded border-dark d-inline-block">
-                <div class="col-12 m-0 p-0 float-left shadow-sm  bg-white rounded border-dark">
-                    <img class="img-fluid rounded" src="images/eventi/<?php echo $evento["ImmagineEvento"];?>"></img>
-                </div>
-                <div class="col-12 m-0 p-0 d-inline-block text-left pl-3 pb-3 pt-3">
-                    <h5 class="mb-0"><?php echo $evento["NomeEvento"];?></h5>
-                    <p class="date font-italic m-0 p-0 mt-1"> <?php echo date("d/m/Y h:m", strtotime(substr($evento["DataEvento"], 0, -3)));?></p>
-                    <p class="m-0 p-0 font-description"><?php echo $evento["Luogo"];?> </p>
-                </div> 
-                <div class="col-12 m-0 p-0">
-                    <div data-IDEvento="<?php echo $evento["IDEvento"]?>"><span class="cuore-pieno text-dark pointer mx-3 fas fa-heart fa-2x"></span></div>
-                    <a href="./evento.php?IDEvento=<?php echo $evento["IDEvento"]?>"><button class="scopri btn py-1 px-3 mx-3 shadow-sm purple-btn rounded-pill">Scopri</button></a>
+            <div class="col-12 col-md-6 col-lg-3 col-xl-3 p-2 ml-0 mt-3 mb-4">
+                <div class="shadow-sm bg-white roundend-corners border-dark d-inline-block p-2">
+                    <div class="col-12 m-0 p-0 float-left shadow-sm  bg-white roundend-corners border-dark">
+                        <img class="img-fluid roundend-corners" src="images/eventi/<?php echo $evento["ImmagineEvento"];?>"></img>
+                    </div>
+                    <div class="col-12 m-0 p-0 d-inline-block text-left px-3 pb-3 pt-3">
+                        <h5 class="mb-0"><?php echo $evento["NomeEvento"];?></h5>
+                        <p class="date font-italic m-0 p-0 mt-1"> <?php echo date("d/m/Y h:m", strtotime(substr($evento["DataEvento"], 0, -3)));?></p>
+                        <p class="m-0 p-0 font-description"><?php echo $evento["Luogo"];?> </p>
+                    </div> 
+                    <div class="col-12 m-0 mb-2">
+                        <div data-IDEvento="<?php echo $evento["IDEvento"]?>"><span class="cuore-pieno text-dark pointer mx-3 fas fa-heart fa-2x"></span></div>
+                        <a href="./evento.php?IDEvento=<?php echo $evento["IDEvento"]?>"><button class="scopri btn py-1 px-3 mx-3 shadow-sm purple-btn rounded-pill">Scopri</button></a>
+                    </div>
                 </div>
             </div>
         <?php endforeach ?>
