47bab432fa9ffa4a1068af33df35cb3211bbb6d9
Martina <martina.baiardi4@studio.unibo.it>
2020-02-13 11:20:23 +0100

    Aggiunta la pagina Eventi

--- Sito/eventi.php
+++ Sito/eventi.php
@@ -2,6 +2,7 @@
         require_once("./bootstrap.php");
         $templateParams["page_content"] = "./template/eventi_content.php";
         $templateParams["categories"] = $dbh->getCategories(); /* For Footer Links */
-    
+        $templateParams["eventi"] = $dbh->getAllEvents();
+        
         require_once("./template/base.php");
 ?>
---
+++ Sito/template/eventi_content.php
@@ -0,0 +1,36 @@
+<div class="row my-5">
+    <div class="col-0 col-md-1"></div>
+        <div class="col-12 col-md-10 p-0 m-0">
+ 
+            <div class="row">
+                <div class="col-8 col-md-7 row">
+                    <h3 class="d-inline mr-3">Eventi</h3>
+                    <p class="text-black-50 pt-2">(<?php echo count($templateParams["eventi"]) ?> risultati)</p>
+                </div>
+            </div>
+            <hr/>
+
+            <div class="row">
+                <?php foreach ($templateParams["eventi"] as $evento) :?>
+                    <div class="col-12 col-md-6 col-lg-3 col-xl-3 p-2 ml-0 mt-3 mb-4">
+                        <div class="shadow-sm bg-white rounded border-dark d-inline-block">
+                            <div class="col-12 m-0 p-0 float-left shadow-sm  bg-white rounded border-dark">
+                                <img class="img-fluid rounded" src="images/eventi/<?php echo $evento["ImmagineEvento"];?>"></img>
+                            </div>
+                            <div class="col-12 m-0 p-0 d-inline-block text-left px-3 pb-3 pt-3">
+                                <h5 class="mb-0"><?php echo $evento["NomeEvento"];?></h5>
+                                <p class="date font-italic m-0 p-0 mt-1"> <?php echo date("d/m/Y h:m", strtotime(substr($evento["DataEvento"], 0, -3)));?></p>
+                                <p class="m-0 p-0 font-description"><?php echo $evento["Luogo"];?> </p>
+                            </div> 
+                            <div class="col-12 m-0 mb-2">
+                                <div data-IDEvento="<?php echo $evento["IDEvento"]?>"><span class="cuore-pieno text-dark pointer mx-3 fas fa-heart fa-2x"></span></div>
+                                <a href="./evento.php?IDEvento=<?php echo $evento["IDEvento"]?>"><button class="scopri btn py-1 px-3 mx-3 shadow-sm purple-btn rounded-pill">Scopri</button></a>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach ?>
+            </div>
+            
+        </div>
+    <div class="col-0 col-md-1"></div>
+</div>
