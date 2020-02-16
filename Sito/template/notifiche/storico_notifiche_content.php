d588bd07b67b5aefde87693a508c79a860e9cb8d
Asia <asia.lucchi@studio.unibo.it>
2020-02-16 16:38:20 +0100

    fix

--- Sito/template/notifiche/storico_notifiche_content.php
+++ Sito/template/notifiche/storico_notifiche_content.php
@@ -2,8 +2,8 @@
   <div class="col-0 col-md-1"></div>
   <div class="col-12 col-md-10">
     <div class="row mb-md-3 mb-0 mt-5 align-items-end">
-        <p class="titolo_sezioni col-8 col-md-7 mt-2 mb-0">Storico notifiche</p>
-        <a class="col-md-5 col-4 text-right pt-3 cursor-pointer purple-black-link font-weight-bold" href="nuove_notifiche.php"> Vai alle nuove notifiche </a>
+        <p class="titolo_sezioni col-7 col-md-7 mt-2 mb-0">Storico notifiche</p>
+        <a class="col-md-5 col-5 text-right pt-3 cursor-pointer purple-black-link font-weight-bold" href="nuove_notifiche.php"> Vai alle nuove notifiche </a>
     </div>
     <hr class="mt-1 mx-2"/>
     <?php if (count($templateParams["notifiche"]) == 0):?>
