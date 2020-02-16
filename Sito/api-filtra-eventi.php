80c15932eb8378aa4c66a2b6db7ff0bb64f89500
Martina <martina.baiardi4@studio.unibo.it>
2020-02-16 20:17:06 +0100

    Aggiunti i link in mockup v2

--- Sito/api-filtra-eventi.php
+++ Sito/api-filtra-eventi.php
@@ -35,9 +35,9 @@ if(isset($_POST["filter"])){
     }
 }
 
-/* foreach ($result as $event) {
-    $event["DataEvento"].replace(date("d/m/Y h:m", strtotime(substr($event["DataEvento"]))));
-}  */
+foreach ($result as $event) {
+    $event["DataEvento"] = date("d/m/Y h:m", strtotime(substr($event["DataEvento"], 0, -3)));
+}  
 header("Content-Type: application/json");
 print_r(json_encode($result));
 
